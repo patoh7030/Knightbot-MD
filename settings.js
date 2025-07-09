6faf6619c65908594f5936e1434bb0d188a49098
patoh7030 <cryptopatrick4@gmail.com>
2025-07-09 04:57:12 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Sir Ricky', // Your name
+  botOwner: 'Ricky', // Your name
   ownerNumber: '25410273994', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
